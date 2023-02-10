d78398f5b5248138515d67d275b65a16730665b2
Marcio <marciosantos.dev@gmail.com>
2023-02-10 12:08:47 -0300

    titulo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Static Badaró AWS.
         </p>
         <a
           className="App-link"
